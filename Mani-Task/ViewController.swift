90c1e6a0f288092dfd3e83ef8e6329fa3b0378ca
Iman Zabihi <iman.zabihi7@gmail.com>
2023-12-22 11:45:18 +0800

    Initial Commit

---
+++ Mani-Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mani-Task
+//
+//  Created by Iman Zabihi on 22/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
